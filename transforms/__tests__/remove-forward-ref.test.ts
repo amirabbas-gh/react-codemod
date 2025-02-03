043fc453a7cb7944e70411d2893f98206077b485
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-02-03 19:23:38 +0330

    fix: fix remove-forward-ref test error

--- transforms/__tests__/remove-forward-ref.test.ts
+++ transforms/__tests__/remove-forward-ref.test.ts
@@ -1,47 +1,40 @@
-
-'use strict';
+"use strict";
 
 const jsTests = [
-  'function-expression',
-  'arrow-function-expression',
-  'forward-ref-import',
-  'forward-ref-import-2',
-  'props-identifier', 
-  'props-object-pattern', 
-  'callee-is-member-expression'
+  "function-expression",
+  "arrow-function-expression",
+  "forward-ref-import",
+  "forward-ref-import-2",
+  "props-identifier",
+  "props-object-pattern",
+  "callee-is-member-expression",
 ];
 
 const tsTests = [
-  'type-arguments', 
-  'type-arguments-custom-names', 
-  'type-arguments-type-literals', 
-  'props-type-literal'
+  "type-arguments",
+  "type-arguments-custom-names",
+  "type-arguments-type-literals",
+  "props-type-literal",
 ];
 
-const defineTest = require('jscodeshift/dist/testUtils').defineTest;
+const defineTest = require("jscodeshift/dist/testUtils").defineTest;
 
-describe('remove-forward-ref', () => {
-
-  jsTests.forEach(test =>
+describe("remove-forward-ref", () => {
+  jsTests.forEach((test) =>
     defineTest(
       __dirname,
-      'remove-forward-ref',
+      "remove-forward-ref",
       null,
-      `remove-forward-ref/${ test }`
+      `remove-forward-ref/${test}`
     )
   );
 
-
-  describe('typescript', () => {
-
+  describe("typescript", () => {
     beforeEach(() => {
-      jest.mock('../remove-forward-ref', () => {
-        return Object.assign(
-          require.requireActual('../remove-forward-ref'),
-          {
-            parser: 'tsx'
-          }
-        );
+      jest.mock("../remove-forward-ref", () => {
+        return Object.assign(jest.requireActual("../remove-forward-ref"), {
+          parser: "tsx",
+        });
       });
     });
 
@@ -49,14 +42,13 @@ describe('remove-forward-ref', () => {
       jest.resetModules();
     });
 
-    tsTests.forEach(test => {
+    tsTests.forEach((test) => {
       defineTest(
         __dirname,
-        'remove-forward-ref',
+        "remove-forward-ref",
         null,
-        `remove-forward-ref/typescript/${ test }`
+        `remove-forward-ref/typescript/${test}`
       );
     });
-    
   });
 });
